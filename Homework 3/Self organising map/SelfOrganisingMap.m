390615ad613f68dff033bf6700bb057240c69f01
maximiliann97 <maximilian@live.se>
2022-10-16 13:17:38 +0200

    Update SelfOrganisingMap.m

--- Homework 3/Self organising map/SelfOrganisingMap.m
+++ Homework 3/Self organising map/SelfOrganisingMap.m
@@ -20,6 +20,7 @@ d_eta = 0.01;   %with decay rate
 sigma = 10;   %Initial width of neighbourhood function
 d_sigma = 0.1;  %with decay rate
 W = rand(40,40,4);
+W_init = W;
 
 
 for epoch = 1:nEpochs
@@ -27,19 +28,26 @@ for epoch = 1:nEpochs
     sigma = sigma * exp(-d_sigma*epoch);
     for input = 1:nInputs
         randomIndex = randi(nInputs);
-        X = data(randomIndex,:);
+        X(1,1,:) = data(randomIndex,:);
         distance = zeros(40);
         for k = 1:length(X) 
             distance = distance + sqrt(W(:,:,k) - X(k)).^2;
         end
         [i_min,j_min]  = find(distance==min(distance(:)));
-        
+        r0 = [i_min j_min];
+
         for i = 1:height(distance)
             for j = 1:length(distance)
-                if distance(i_min,j_min) < sigma/3
-                    
+                r = [i j];
+                distance_r0 = vecnorm(r-r0);
+                
+                if distance_r0 < sigma/3
+                    h = exp(-(1/2*sigma^2) * distance_r0);
+                    dW = eta*h*(X-W(i,j,:));
+                    W(i,j,:) = W(i,j,:) + dW;
                 end
             end
         end
     end
 end
+
